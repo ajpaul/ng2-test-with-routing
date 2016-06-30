506deda1dc75eb58342173c17287787d696289ca
Paul <ajp01@acuitysso.com>
2016-06-30 15:10:48 -0400

    Added some sample ng2-material components to device management

--- src/app/deviceManagement/deviceManagement.component.ts
+++ src/app/deviceManagement/deviceManagement.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MdCard } from '@angular2-material/card';
+import { MdButton } from '@angular2-material/button';
+import { MdInput } from '@angular2-material/input';
 
 @Component({
-    selector: 'device-management',
-    template:
-    `
-    <h2>Welcome to the {{ title }}</h2>
-    `
+  selector: 'device-management',
+  templateUrl: '../src/app/deviceManagement/deviceManagement.component.html',
+  styleUrls: ['../src/app/deviceManagement/deviceManagement.component.css'],
+  directives: [MdButton, MdCard, MdInput]
 })
 
-export class DeviceManagementComponent {
+export class DeviceManagementComponent implements OnInit {
     title: string = 'Device Management'; 
+    buttonText: string = 'Click me and something will happen!'
+
+    //this is a lifecycle hook?
+    ngOnInit(): void {
+      console.log('ngOnInit() called for deviceManagement');
+    }
+
+    doSomething(): void {
+        alert("something");
+    }
 }
